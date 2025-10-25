a3fbc2c7b0c25810bddfc1c2d793c00f8a2092a0
HridyanshSingh14 <hridyanshsingh2006@outlook.com>
2025-10-25 16:43:44 +0530

    Update Project.sol
    Wrote a small contract showing the project name and description

--- contracts/Project.sol
+++ contracts/Project.sol
@@ -1,6 +1,20 @@
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
 
-pragma solidity >=0.8.2 <0.9.0;
+contract Identity {
+    string Project;
+    string Description;
 
-contract NFT-Marketplace-with-Royalties {
+    constructor() {
+        Project = "NFT-Marketplace-with-Royalties";
+        Description = unicode"An NFT Marketplace with Royalties is a decentralized platform that enables users to mint, buy, and sell digital assets while automatically rewarding creators with royalties on every resale. It ensures fair revenue sharing, transparent transactions, and long-term value for artists through smart contract automation.";
+    }
+    
+    function showProject() public view returns (string memory) {
+        return Project;
+    }
+    
+    function showDescription() public view returns (string memory) {
+        return Description; 
+    }
 }
