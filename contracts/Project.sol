87c72d2acd5b9a34c4b503beee032bcbedc81f3b
HridyanshSingh14 <hridyanshsingh2006@outlook.com>
2025-10-26 20:07:54 +0530

    Update Project.sol
    added startdate

--- contracts/Project.sol
+++ contracts/Project.sol
@@ -4,10 +4,12 @@ pragma solidity >=0.5.0 <0.9.0;
 contract Identity {
     string Project;
     string Description;
+    int startdate;
 
     constructor() {
         Project = "NFT-Marketplace-with-Royalties";
         Description = unicode"An NFT Marketplace with Royalties is a decentralized platform that enables users to mint, buy, and sell digital assets while automatically rewarding creators with royalties on every resale. It ensures fair revenue sharing, transparent transactions, and long-term value for artists through smart contract automation.";
+        startdate = 23102006;
     }
     
     function showProject() public view returns (string memory) {
@@ -16,5 +18,8 @@ contract Identity {
     
     function showDescription() public view returns (string memory) {
         return Description; 
+    }
+    function showstartDate() public view returns (int memory) {
+        return startdate; 
     }
 }
